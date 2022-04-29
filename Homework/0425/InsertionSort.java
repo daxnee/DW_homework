e8e0df1c3c3e349e63b61aae7f1a9f20bae1844c
YangDaEun <das03041@gmail.com>
2022-04-29 15:44:26 +0900

    fix : 코드 수정

--- Homework/0425/InsertionSort.java
+++ Homework/0425/InsertionSort.java
@@ -1,22 +1,18 @@
 package 삽입정렬;
 
-public class InsertionSort {
-	
-		public static void main(String[] args) {
-		int array[] = {8, 5, 6, 2, 4};
-		int temp = 0;
-		int len = array.length;
-		 //삽입정렬을 이용하여 오름차순 정렬!
-		for(int i=1; i<len; i++) {
-			int key = array[i];
-			for(int j=i-1; j>=0 && array[j]<array[i]; j--) {
-				if(key > array[j]) {
-					// -------------
-					array[j+1] = array[j];// temp == array[1]
-					array[j+1] = key;
-					}
-				System.out.println(array[j]);
-			 	}
-			}
-}
-}
+int array[] = {8, 5, 6, 2, 4};
+		 
+		 for(int i=0; i<array.length-1; i++) {
+			 for(int j=i+1; j>0; j--) {
+				 int key = j;
+				 if(array[key]< array[j-1]) {
+					 int temp = array[key];
+					 array[key] = array[j-1];
+					array[j-1] = temp;
+				 }
+			 }
+		 }
+		 for (int i : array) {
+			 System.out.println(i);
+		 }
+		 
